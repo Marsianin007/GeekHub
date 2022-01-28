a6bbc7a2e036812693b1feed0bce7f67ca7356b1
Vlad Chernoborodiuk <45739207+Marsianin007@users.noreply.github.com>
2022-01-28 20:24:02 +0200

    new v

--- HT_18/code.py
+++ HT_18/code.py
@@ -2,53 +2,51 @@ import requests
 import csv
 import sys
 
+class RequestsParse(object):
 
-category_list = ["askstories", "showstories", "newstories", "jobstories"]
-if len(sys.argv) > 1:
-    category_from_user = sys.argv[1]
-else:
-    category_from_user = "newstories"
-if category_from_user not in category_list:
-    print("Такої категорії немає...")
-    raise SystemExit()
+    @staticmethod
+    def check_category():
+        category_list = ["askstories", "showstories", "newstories", "jobstories"]
+        if len(sys.argv) > 1:
+            category_from_user = sys.argv[1]
+        else:
+            category_from_user = "newstories"
+        if category_from_user not in category_list:
+            print("Такої категорії немає...")
+            raise SystemExit()
 
-id_list = requests.get(f"https://hacker-news.firebaseio.com/v0/"
-                       f"{category_from_user}.json?print=pretty").json()
+        RequestsParse.id_list = requests.get(f"https://hacker-news.firebaseio.com/v0/"
+                               f"{category_from_user}.json?print=pretty").json()
 
-all_keys_list = []
+        return category_from_user, RequestsParse.id_list
 
-for id in id_list:
-    try:
-        page_keys = requests.get(f"https://hacker-news.firebaseio.com/v0/"
-                                 f"item/{id}.json?print=pretty").json()
-        for key in page_keys:
-            if key not in all_keys_list:
-                all_keys_list.append(key)
-    except:
-        pass
+    @staticmethod
+    def take_values():
+        dict_to_csv = {}
+        category_from_user, id_list = RequestsParse.check_category()
+        for id in id_list:
+            for key in dict_to_csv:
+                dict_to_csv[key] = "null"
+            list_to_csv = []
+            try:
+                page = requests.get(f"https://hacker-news.firebaseio.com/v0/"
+                                         f"item/{id}.json?print=pretty").json()
+                for key in page.keys():
+                    if key not in dict_to_csv.keys():
+                        dict_to_csv.update({key: "null"})
+                        dict_to_csv[key] = page[key]
+                    else:
+                        dict_to_csv[key] = page[key]
+                for value in dict_to_csv.values():
+                    list_to_csv.append(value)
+                with open(f"{category_from_user}.csv", 'a', newline='', encoding='utf-8') as f:
+                         writer = csv.writer(f, dialect='excel', delimiter=';')
+                         writer.writerow(list_to_csv)
+            except:
+                pass
 
-with open(f"{category_from_user}.csv", 'a', newline='', encoding='utf-8') as f:
-    writer = csv.writer(f, dialect='excel', delimiter=';')
-    writer.writerow(all_keys_list)
-
-main_dict = {}
-for key in all_keys_list:
-    tmp_dict = {key: "null"}
-    main_dict.update(tmp_dict)
-
-for id in id_list:
-    list_to_csv = []
-    dict_to_csv = main_dict.copy()
-    try:
-        page = requests.get(f"https://hacker-news.firebaseio.com/v0/"
-                            f"item/{id}.json?print=pretty").json()
-        for key in page.keys():
-            dict_to_csv[key] = page[key]
-        for i in dict_to_csv.keys():
-            list_to_csv.append(dict_to_csv[i])
         with open(f"{category_from_user}.csv", 'a', newline='', encoding='utf-8') as f:
             writer = csv.writer(f, dialect='excel', delimiter=';')
-            writer.writerow(list_to_csv)
-        print("write done")
-    except:
-        pass
+            writer.writerow(dict_to_csv.keys())
+
+RequestsParse.take_values()
